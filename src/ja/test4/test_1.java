7fa5f4286762841f497e030cdf604101a0cafc92
WhatLLL <1280936498@qq.com>
2025-10-30 17:02:35 +0800

    struct java-test4

---
+++ src/ja/test4/test_1.java
@@ -0,0 +1,17 @@
+package ja.test4;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class test_1 {
+    public static void main(String[] args) throws SQLException {
+        Connection conn =  new DbConfig().dbConfig();
+        String sql = "select * from sample";
+        ResultSet rs = conn.createStatement().executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getString("sample_name"));
+
+        }
+    }
+}
